611aca6c6ad101ff98e0bc148e96a85f6262c583
moonseokho85 <mseokho7733@gmail.com>
2020-02-11 10:01:29 +0900

    modify files about details

--- django_mldl/site_1/lotto/views.py
+++ django_mldl/site_1/lotto/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse # 추가됨
 # Create your views here.
 
 def index(request):
--- django_mldl/site_1/site_1/urls.py
+++ django_mldl/site_1/site_1/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from lotto import views
+from lotto import views # 추가됨
 
 urlpatterns = [
     path('admin/', admin.site.urls), # 127.0.0.1:8000/admin/
