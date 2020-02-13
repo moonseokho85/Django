de007ce51f9015f7443e95d98466298bb2d06b3f
moonseokho85 <mseokho7733@gmail.com>
2020-02-13 11:00:19 +0900

    detail page sync with templates & make up 404 error page

--- django_mldl/site_2/polls/views.py
+++ django_mldl/site_2/polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Question
 
 # Create your views here.
@@ -13,7 +13,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question {}.".format(question_id))
+    try:
+        q = Question.objects.get(pk=question_id)
+    except:
+        raise Http404
+    return render(request, 'polls/detail.html', {'question': q})
 
 def results(request, question_id):
     response = "You're looking at the results of question {}."
