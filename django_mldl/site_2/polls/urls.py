1d4ad81b54345dc42e3944a6f7d77a8ba5cb7b28
moonseokho85 <mseokho7733@gmail.com>
2020-02-13 10:24:38 +0900

    make up multiple page

--- django_mldl/site_2/polls/urls.py
+++ django_mldl/site_2/polls/urls.py
@@ -4,4 +4,13 @@ from . import views
 urlpatterns = [
     # 127.0.0.1:8000/polls/" 이후의 URL 은 polls/urls.py 가 handling 하도록 만들 예정입니다
     path('', views.index, name='index'), # '127.0.0.1:8000/polls/' 를 받아내도록 만들어줄 것입니다
+
+    # 127.0.0.1:8000/polls/3
+    path('<int:question_id>/', views.detail, name='detail'),
+
+    # 127.0.0.1:8000/polls/3/results
+    path('<int:question_id>/results/', views.results, name='results'),
+
+    # 127.0.0.1:8000/polls/3/vote
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
