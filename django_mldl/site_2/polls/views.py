6fb0123aafb16f56c9045d9b13768e374996cc6e
moonseokho85 <mseokho7733@gmail.com>
2020-02-13 10:35:44 +0900

    Main_page synchronize with templates

--- django_mldl/site_2/polls/views.py
+++ django_mldl/site_2/polls/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello world')
+    # order_ pub_date')[:5] : 등록 날짜 기준 내림차순 정렬 후 앞에서 5 개까지만
+    latest_question_list = Question.objects.order_by('-pub_date')[:5] # '-': 역순
+
+    # 지난 실습에서 render() 함수의 {~:~} 로 html 파일에게 넘겨주던 dict 를 context 라고 부릅니다
+    context = {'latest_question_list': latest_question_list}
+
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question {}.".format(question_id))
