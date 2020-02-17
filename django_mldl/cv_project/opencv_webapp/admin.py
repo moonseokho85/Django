e679313b7e17a27c5defdd83ccbc74a645b4d0a1
moonseokho85 <mseokho7733@gmail.com>
2020-02-17 15:26:27 +0900

    apply opencv in web site

--- django_mldl/cv_project/opencv_webapp/admin.py
+++ django_mldl/cv_project/opencv_webapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import ImageUploadModel
+
+class image_upload_Admin(admin.ModelAdmin):
+    list_display = ('description', 'document')
+
+admin.site.register(ImageUploadModel, image_upload_Admin)
--- django_mldl/cv_project/opencv_webapp/views.py
+++ django_mldl/cv_project/opencv_webapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import SimpleUploadForm, ImageUploadForm
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
+from .cv_functions import cv_detect_face
 
 # Create your views here.
 def first_view(request):
@@ -32,7 +33,8 @@ def detect_face(request):
             post = form.save(commit=False)
             post.save()
 
-            imageURL = settings.MEDIA_URL + form.instance.document.urls
+            imageURL = settings.MEDIA_URL + form.instance.document.name
+            cv_detect_face(settings.MEDIA_ROOT_URL + imageURL)
 
             return render(request, 'opencv_webapp/detect_face.html', {'form': form, 'post': post})
 
