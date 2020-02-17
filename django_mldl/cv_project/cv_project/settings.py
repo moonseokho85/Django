6d813ee97746efe40a8bb7e0e533456b212222a6
moonseokho85 <mseokho7733@gmail.com>
2020-02-17 11:22:03 +0900

    upload image in web

--- django_mldl/cv_project/cv_project/settings.py
+++ django_mldl/cv_project/cv_project/settings.py
@@ -115,8 +115,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images, Favicon용 이미지) 정적 파일
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- django_mldl/cv_project/opencv_webapp/views.py
+++ django_mldl/cv_project/opencv_webapp/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SimpleUploadForm
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 def first_view(request):
     return render(request, 'opencv_webapp/first_view.html')
+
+def simple_upload(request):
+    if request.method == 'POST':
+        form = SimpleUploadForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            myfile = request.FILES['image']
+            fs = FileSystemStorage()
+            filename = fs.save(myfile.name, myfile)
+
+            uploaded_file_url = fs.url(filename)
+
+            context = {'form': form, 'uploaded_file_url': uploaded_file_url}
+            return render(request, 'opencv_webapp/simple_upload.html', context)
+    else:
+        form = SimpleUploadForm()
+        context = {'form': form}
+        return render(request, 'opencv_webapp/simple_upload.html', context)
